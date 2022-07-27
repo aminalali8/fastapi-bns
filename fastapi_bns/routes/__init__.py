03fc432b69010ce673b37ed6e77544e85ed16e8f
Amin Al-Ali Al-Darwish <amin@bunnyshell.com>
2022-07-27 15:53:13 +0100

    Update __init__.py

--- fastapi_bns/routes/__init__.py
+++ fastapi_bns/routes/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import os 
 
 from .content import router as content_router
 from .security import router as security_router
@@ -10,7 +11,10 @@ main_router.include_router(user_router, prefix="/user", tags=["user"])
 main_router.include_router(content_router, prefix="/content", tags=["content"])
 main_router.include_router(security_router, tags=["security"])
 
+if os.environ['REGISTRAR_ID'] is None:
+    os.environ['REGISTRAR_ID'] = 'UNDEFINED'
+    
 
 @main_router.get("/")
 async def index():
-    return {"message": "Hello World!"}
+    return {"message": "Hello World! (From Registrar: " + os.environ['REGISTRAR_ID'] + ")"}
