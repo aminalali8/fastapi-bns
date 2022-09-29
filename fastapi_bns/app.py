dc81c33a6eac710b74b6be498fcc8b483d093802
Amin Al-Ali Al-Darwish <amin@bunnyshell.com>
2022-09-29 17:05:20 +0100

    Update app.py

--- fastapi_bns/app.py
+++ fastapi_bns/app.py
@@ -23,7 +23,8 @@ def read(*paths, **kwargs):
 
 
 description = """
-Bunnyshell; Automate, streamline, and optimize your development cycles! 🚀  Lets get productive! test
+Bunnyshell; Automate, streamline, and optimize your development cycles! 🚀  Lets get productive!
+This is an ephemeral.
 """
 
 app = FastAPI(
